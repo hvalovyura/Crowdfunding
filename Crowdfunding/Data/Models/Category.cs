9b10765852ac802a0c188b282fdbf725ec829b1e
Yura <hvalovyura@gmail.com>
2020-02-03 19:26:50 +0300

    Adding 'Project' and 'Category' models

---
+++ Crowdfunding/Data/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crowdfunding.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        string CategoryName { get; set; }
+
+
+    }
+}
---
+++ Crowdfunding/Data/Models/Project.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crowdfunding.Data.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string LongDescription { get; set; }
+
+        public string Img { get; set; }
+
+        public bool IsFavourite { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
